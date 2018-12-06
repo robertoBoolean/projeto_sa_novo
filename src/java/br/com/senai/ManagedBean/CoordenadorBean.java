cd472a2fbdc8f27a80da45c33638abb54934266f
Ricardo Rossi da Silva <rossi.ricardo1@hotmail.com>
2018-12-05 22:37:45 -0200

    Liberacao

--- src/java/br/com/senai/ManagedBean/CoordenadorBean.java
+++ src/java/br/com/senai/ManagedBean/CoordenadorBean.java
@@ -5,24 +5,47 @@ import br.com.senai.dao.CoordenadorDao;
 import java.util.ArrayList;
 import java.io.Serializable;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-    
+import javax.faces.context.FacesContext;
+import org.primefaces.PrimeFaces;
+
 @ManagedBean(name = "coordenadorBean")
 @SessionScoped
-public class CoordenadorBean implements Serializable{
+public class CoordenadorBean implements Serializable {
+
     private Coordenador coordenador = new Coordenador();
     private CoordenadorDao CoordenadorDao = new CoordenadorDao();
     private List<Coordenador> coordenadores = CoordenadorDao.getAll();
-    
-    public String adicionarCoordenador(){
+
+    private String usuario;
+    private String senha;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String adicionarCoordenador() {
         CoordenadorDao.salvar(coordenador);
         coordenador = new Coordenador();
         coordenadores = CoordenadorDao.getAll();
         return "gerenciar-coordenadores.xhtml";
     }
-    
-     public String cancelarCoordenador(){
+
+    public String cancelarCoordenador() {
         coordenador = new Coordenador();
         return "gerenciar-coordenadores.xhtml";
     }
@@ -43,16 +66,31 @@ public class CoordenadorBean implements Serializable{
         this.coordenadores = coordenadores;
     }
 
-     
-    public String editCoordenador(){
+    public String editCoordenador() {
         return "editar-coordenadores.xhtml";
     }
-    public String excluirCoordenador(){
+
+    public String excluirCoordenador() {
         CoordenadorDao.excluir(coordenador);
         coordenadores = CoordenadorDao.getAll();
         coordenador = new Coordenador();
         return "gerenciar-coordenadores.xhtml";
     }
-    
-}
 
+    public void login() {
+        FacesMessage message = null;
+        boolean loggedIn = false;
+        for (Coordenador coordenacao : coordenadores) {
+            if (usuario != null && usuario.equals(coordenacao.getUsuario()) && senha != null && senha.equals(coordenacao.getSenha())) {
+                loggedIn = true;
+                message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", usuario);
+            } else {
+                loggedIn = false;
+                message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Atenção!", "Credenciais incorretas");
+            }
+        }
+
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        PrimeFaces.current().ajax().addCallbackParam("loggedIn", loggedIn);
+    }
+}
